732b985947e29b1e53ea773760434b5d843c7a07
julesyan <julia.yan@mail.utoronto.ca>
2016-06-26 18:36:02 -0400

    base files for A2

---
+++ findname.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main (int argc, char** argv)
+{
+    // If there are not enough arguments
+    if (argc < 3){
+        printf("usage: fileName, directory names to search through")
+    }
+}
---
+++ myfgrep.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+    int main(int argc, char** argv)
+    {
+		printf("something\n");
+		return 0;
+        // Determine the options
+        // Check if string is in another string
+            // Output the match string if option is enabled
+            // Output the files that have match if option enabled
+    }
