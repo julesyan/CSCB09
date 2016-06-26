3b950a3d90b1afd900e93b69ebb536b0f7f43b5d
julesyan <julia.yan@mail.utoronto.ca>
2016-06-26 18:54:59 -0400

    testing

--- myfgrep.c
+++ myfgrep.c
@@ -1,10 +1,27 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <unistd.h>
+#define true 1
+#define false 0
 
     int main(int argc, char** argv)
     {
-		printf("something\n");
-		return 0;
-        // Determine the options
+		// Go thru each option
+		int optionL = false;
+		int optionH = false;
+		int optionM = false;
+		char c;
+		while ((c = getopt(argc, argv, "hlm::")) != -1){
+			if (c == 'h'){
+				printf("option h");
+				optionH = true;
+			} else if (c == 'l'){
+				printf("option l");
+				optionL = true;
+			} else if (c == 'm'){
+				printf("option m");
+				optionM = true;
+		}
+
         // Check if string is in another string
             // Output the match string if option is enabled
             // Output the files that have match if option enabled
