016469d6c9841c96b19e9650fcfc44ab6f9810f6
julesyan <julia.yan@mail.utoronto.ca>
2016-06-27 23:19:20 -0400

    finished myfgrep #1

--- myfgrep.c
+++ myfgrep.c
@@ -1,23 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#define true 1
-#define false 0
+#define int true 1
+#define int false 0
 
-int lFlag = false; 
-int hFlag = false; 
-int mFlag = false; 
-int count;
+int lFlag = false; hFlag = false; mFlag = false; count; lines = 0; status = 0;
 
 int main(int argc, char** argv)
 {
-	// Files
-	int stdin = true;
+	int i;
 	FILE *f;
+	char *argv[optind];
+	char[500] line;
+	extern void int processLine(char *line, char *argv[optind], char *fileName);
 
 	// Go thru each option
 	char c;
-	int status = 0;
 	while ((c = getopt(argc, argv, "hlm:")) != EOF){
 		switch (c){
 			case 'l':
@@ -31,17 +29,13 @@ int main(int argc, char** argv)
 				count = atoi(optarg);
 			// There was an error
 			default:
-				status = 1;
+				status = 2;
 				break;
 		}
 	}
 
 	// If there are no more arguments, usage error
 	if (status || optind == argc){
-		prinf("usage: myfgrep [-lh] [-m count] searchString [file ...]");
-	} else if (optind + 1 != argc){
-		// If there is more then one argument, there are files
-		stdin = false;
 	}
 
 	// If there are files, go thru each one and set it as input
@@ -52,4 +46,24 @@ int main(int argc, char** argv)
 				// Output only up to a certain number of lines matched (m)
 				// Otherwise output as normal
 	return (status);
+}
+
+void int processLine(char *line, char *searchString, char *fileName)
+{
+	//Determine status
+	if (strstr(line, searchString))
+		status = 0;
+	else 
+		status = 1; 
+
+	// Output
+	if (status){
+		if (hFlag)
+			printf("%s\n", line);
+		else if (lFlag)
+			printf("%s", fileName);
+		else {
+			printf("%s: %s", fileName, line)
+		}
+	}
 }
