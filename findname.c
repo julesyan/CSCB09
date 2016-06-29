11667574502c43df709e5a52f1d41706ad8c68ed
julesyan <julia.yan@mail.utoronto.ca>
2016-06-29 09:32:34 -0400

    initial start up

--- findname.c
+++ findname.c
@@ -2,8 +2,17 @@
 
 int main (int argc, char** argv)
 {
+     char searchString[500];
+    char path[2000];
+    int i;
+
     // If there are not enough arguments
     if (argc < 3){
-        printf("usage: fileName, directory names to search through")
-    }
+        printf("usage: findname name dir ...")
+    } else {
+        // Get the search string
+        searchString = argv[1];
+
+        // For each directory given
+
 }
