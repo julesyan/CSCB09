17033eeaf69d4e40ee1deb30e66a42afe49bc943
julesyan <julia.yan@hotmail.com>
2016-06-30 15:40:18 -0400

    finished dungeon

--- dungeon.c
+++ dungeon.c
@@ -6,41 +6,38 @@
 #define FALSE 1
 #define DEFAULT "pass"
 char *map[DUNGEONSIZE][DUNGEONSIZE];
-int w = 0; h = 0;
+int w = 0, h = 0;
 
 void printMap(){
-    int i = 0; j = 0;
-    for (i = 0; i < DUNGEONSIZE; i++){
-        for (j = 0; j < DUNGEONSIZE; j++){
-            printf("%s\t", map[i][j]);
-        }
+    int i = 0, j = 0;
+    for (; i < DUNGEONSIZE; i++){
+        for (; j < DUNGEONSIZE; j++)
+            printf("%s\t", map[j][i]);
         printf("\n");
     }
 }
 
 int isDefault()
 {
-    // Check outside of bounds
-    if (h < 0)
+    // Check outside of bounds and return if in default room
+    if (h < 0) 
         h = DUNGEONSIZE - 1;
-    else if (h = DUNGEONSIZE)
+    else if (h == DUNGEONSIZE)
         h = 0;
     if (w < 0)
         w = DUNGEONSIZE - 1;
-    else if (w = DUNGEONSIZE)
+    else if (w == DUNGEONSIZE)
         w = 0;
-
-    if (map[x][y] == DEFAULT)
-        return TRUE;
-    return FALSE;
+    if (strcmp(map[w][h], DEFAULT))
+        return FALSE;
+    return TRUE;
 }
 
 
 int main(int argc, char **argv)
 {
     FILE *f;
-    char line[500]; *t; 
-    int counter;
+    char line[500], *t, c;
 
     if (argc != 2){
         fprintf(stderr, "usage: %s mapfile", argv[0]);
@@ -57,14 +54,13 @@ int main(int argc, char **argv)
         for (t = strtok(line, " \t\n"); w < DUNGEONSIZE; 
             t = strtok(NULL, " \t\n")){
             // If no token present, set to default, otherwise add it in to map
-            if (t == NULL){
+            if (t == NULL)
                 t = DEFAULT;
-            }
-            if ((map[i][j] = malloc(strlen(t) + 1)) == NULL) {
+            if ((map[w][h] = malloc(strlen(t) + 1)) == NULL) {
                 fprintf(stderr, "out of memory\n");
                 exit(1);
             }
-            strcpy(map[i][j], t);
+            strcpy(map[w][h], t);
             w++;
         }
         h++;
@@ -72,42 +68,32 @@ int main(int argc, char **argv)
     }
     fclose(f);
 
-    /*
-    NOTES:
-        - When asking user for input is: "You are at: <room>\n"
-        - When getting input only looks at first character even if there are 
-            other characters there (e.g. exit becomes e) DOES NOT TAKE IN 
-            MULTIPLE CMDS AT ONCE (i.e. esn)
-        - if room=pass then goes to next available
-        - if is reach the edge go back to beginning (i.e. top and left)
-        - continuous input
-        - if not recognized input then "Type n, w, e, s, or m.\n"
-
-    */    
+    // Getting input from user to move around the map   
     w = 0; h = 0;
-    printf("You are at room: %s\n", map[w, h]);
+    printf("You are at room: %s\n", map[w][h]);
     while (fgets(line, 500, stdin)){
-        t = line[0];
+        c = line[0];
         switch(c){
             case 'n':
-                for (h--; isDefault(); h--)
+                for (h--; isDefault() == TRUE; h--) {}
                 break;
             case 'e':
-                for (w++; isDefault(); w++)
+                for (w++; isDefault() == TRUE; w++) {}
                 break;
             case 's':
-                for (h++; isDefault(); h++)
+                for (h++; isDefault() == TRUE; h++) {}
                 break;
             case 'w':
-                for (w--; isDefault(); w--)
+                for (w--; isDefault() == TRUE; w--) {}
                 break;
             case 'm':
                 printMap();
                 break;
             default:
-                printf("\nType n, w, e, s, or m\n")
+                printf("Type n, w, e, s, or m\n");
                 break;
         }
-        printf("You are at room: %s\n", map[w, h]);
+        printf("You are at room: %s\n", map[w][h]);
     }
+    return(0);
 }
