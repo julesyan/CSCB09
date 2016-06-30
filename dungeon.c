483c74b49f9c1a326e6e083a9558dd2c3b10c377
julesyan <julia.yan@hotmail.com>
2016-06-30 00:17:36 -0400

    started dungeon

--- dungeon.c
+++ dungeon.c
@@ -1,6 +1,21 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <string.h>
+#define DUNGEONSIZE 10
+char *map[DUNGEONSIZE][DUNGEONSIZE];
 
-int main(int argc, char** argv)
+
+int main(int argc, char **argv)
 {
+    if (argc != 2){
+        fprintf(stderr, "usage: %s file", argv[0]);
+        return(1);
+    }
+
     
+            if ((map[i][j] = malloc(strlen(t) + 1)) == NULL) {
+                fprintf(stderr, "out of memory\n");
+                exit(1);
+            }
+            strcpy(map[i][j], t);
+
 }
