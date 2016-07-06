55eca4772deb30687f8376286726bb49c03b4eed
Julia Yan <yanjuli1@iits-b473-20045.(none)>
2016-07-06 14:42:31 -0400

    fork finished

--- tut8/fork.c
+++ tut8/fork.c
@@ -2,9 +2,18 @@
 #include <unistd.h>
 #include <sys/types.h>
 
+void childProcess(int i){ 
+    int sum, j;
+    // Sums up from 5i to 5i+4
+    for (sum = j = 0; j < 5, j++){
+        sum += (5*i) + j;
+    }
+    exit(sum);
+}
+
 int main ()
 {
-    int x, i;
+    int sum, x, i;
     // Create 5 children proccess
     for (i = 0; i < 5; i++){
         x = fork();
@@ -15,10 +24,17 @@ int main ()
             return(1);
         } else if (x == 0){
             // Comput some of integers 5i through 5i + 4
-            return(5*x);
+            childProcess(i);
         }
     }
 
-    // Parent now waits
+    // Parent (for each parent created)
+    for (sum = i = 0; i < 5; i++){
+        // Waits for child
+        wait(&status);
 
+        // sums the total exit statuses
+        // ***WHAT IS >> 8***
+        sum += (status >> 8);
+    }
 }
