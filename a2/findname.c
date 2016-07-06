1f3dc9e7d2511338851e2291db703099a72e65d2
Julia Yan <yanjuli1@iits-b473-20045.(none)>
2016-07-06 14:27:31 -0400

    moved files

---
+++ a2/findname.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <string.h>
+#define FALSE 1
+#define TRUE 0
+    
+const char *searchString;
+
+int printFound(char *cur)
+{
+    DIR *dp;
+    struct dirent *p;
+    struct stat statbuf;
+    char path[2000];
+    int found = FALSE;
+
+    // Check directory is ok
+    if ((dp = opendir(cur)) == NULL){
+        perror(cur);
+        return(2);
+    }
+
+    // Read directory if there have been no errors
+    while ((p = readdir(dp)) && (found == FALSE || found == TRUE)){
+        // Get the path of the current file/directory
+        strcpy(path, cur);
+        strcat(path, "/");
+        strcat(path, p->d_name);
+
+        // If something wrong with file
+        if (stat(path, &statbuf)){
+            perror(p->d_name);
+            return(2);
+        }
+
+        // If the directory is a directory
+        if ((S_ISDIR(statbuf.st_mode))){
+            // Ignore the . and .. cases
+            if ((strcmp(p->d_name, ".")) == 0 || (strcmp(p->d_name, "..")) == 0)
+                continue;
+            found = printFound(path); 
+        } else {
+            // If the names match
+            if (strcmp(p->d_name, searchString) == 0){
+                printf("%s\n", path);
+                found = TRUE;
+            }
+        }              
+    }
+    closedir(dp);
+    return(found);
+}
+
+int main (int argc, char** argv)
+{
+    int i;
+    int found = FALSE;
+
+    if (argc < 3){
+        fprintf(stderr, "usage: %s name dir ...", argv[0]);
+        return(2);
+    } else {
+        // Get the search string
+        searchString = argv[1];
+
+        // For each directory given
+        for (i = 2; i < argc; i++){
+            // If error happened, exit
+            found = printFound(argv[i]);
+            if (found != FALSE && found != TRUE)
+                return(2);
+        }
+    }
+    return (found);
+}
---
+++ a2/myfgrep.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#define FALSE 1
+#define TRUE 0
+
+int lFlag = FALSE, hFlag = FALSE, mFlag = FALSE, count, lines = 0, status = 0,
+    found = FALSE, lotsOfFiles = FALSE, finishedFile = FALSE;
+
+int main(int argc, char **argv)
+{
+    int i;
+    FILE *f;
+    char line[500];
+    extern void processLine(char *line, char *searchString, char *fileName);
+
+    // Go thru each option
+    char c;
+    while ((c = getopt(argc, argv, "hlm:")) != EOF){
+        switch (c){
+            case 'l':
+                lFlag = TRUE;
+                break;
+            case 'h':
+                hFlag = TRUE;
+                break;
+            case 'm':
+                mFlag = TRUE;
+                count = atoi(optarg);
+                break;
+            // There was an error
+            default:
+                status = 2;
+                break;
+        }
+    }
+
+    // If there are no more arguments, usage error
+    if (status || optind == argc){
+        printf("usage: myfgrep [-lh] [-m count] searchString [file ...]\n");
+    } else {    
+        // If no files, standard input
+        if (argc == optind + 1){
+            while (fgets(line, 500, stdin) != NULL && 
+                (mFlag == FALSE || lines < count)){
+                processLine(line, argv[optind], "stdin");
+            }
+        } else {
+            if (argc > optind + 2) { lotsOfFiles = TRUE; }
+            // If there are no more arguments process standard input
+            for (i = optind + 1; i < argc && status != 2; i++){
+                // If standard input
+                if (!(strcmp(argv[i], "-"))){
+                    while (fgets(line, 500, stdin) != NULL && 
+                        (mFlag == FALSE || lines < count))
+                        processLine(line, argv[optind], "stdin");
+                } else {
+                    // Open file 
+                    f = fopen(argv[i], "r");
+                    finishedFile = FALSE;
+                    if (f == NULL){
+                        perror(argv[i]);
+                        status = 2;
+                    }
+                    // Process each line
+                    while (fgets(line, 500, f) != NULL  && 
+                        (mFlag == FALSE || lines < count) && 
+                        finishedFile == FALSE)
+                        processLine(line, argv[optind], argv[i]);
+                    fclose(f);
+                }            
+            }
+        }
+    }
+    if (status != FALSE && status != TRUE)
+        return status;
+    else
+        return found;
+}
+
+void processLine(char *line, char *searchString, char *fileName)
+{
+    //Determine status
+    if (strstr(line, searchString)){
+        found = TRUE;
+        if (hFlag == TRUE || lotsOfFiles == FALSE)
+            printf("%s", line);
+        else if (lFlag == TRUE){
+            printf("%s\n", fileName);
+            finishedFile = TRUE;
+        }
+        else {
+            printf("%s: %s", fileName, line);
+        }
+        if (mFlag == TRUE) { lines ++; }
+    }
+}
