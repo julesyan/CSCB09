3711cd60cda143051ca3a2478d9870c19a3594bb
julesyan <julia.yan@hotmail.com>
2016-07-17 16:46:57 -0400

    step 8 completed

--- a3/fsh.c
+++ a3/fsh.c
@@ -46,49 +46,87 @@ void execute(struct parsed_line *p)
 
     fflush(stdout);
     switch (fork()) {
-    case -1:
-    	perror("fork");
-    	laststatus = 127;
-    	break;
-    case 0:
-    	/* child */
-    	execute_one_subcommand(p);
-    	break;
-    default:
-    	/* parent */
-    	wait(&status);
-    	laststatus = status >> 8;
+        case -1:
+        	perror("fork");
+        	laststatus = 127;
+        	break;
+        case 0:
+        	/* child */
+            execute_one_subcommand(p);
+        	break;
+        default:
+        	/* parent */
+        	wait(&status);
+        	laststatus = status >> 8;
     }
 }
 
+struct pipeline * execute_pipe(struct pipeline *pl)
+{
+    int pipefd[2];
+
+    /* Get a pipe for this cmd */
+    if (pipe(pipefd)){
+        perror("pipe");
+        exit(127);
+    }
+
+    /* Executing two cmds, one in each side of pipe */
+    switch(fork()){
+        case -1:
+            perror("fork");
+            laststatus = 127;
+            break;
+        case 0:
+            /* child */
+            /*  1. Close the input
+                2. Have the pipe output as the new output
+                3. Clean up the pipe output */
+            close(pipefd[0]);
+            dup2(pipefd[1], 1);
+            close(pipefd[1]);
+            return pl;
+        default:
+            /* parent */
+            /*  1. Close the output
+                2. Have pipe output from new input
+                3. clean up the pip output */
+            close (pipefd[1]);
+            dup2(pipefd[0], 0);
+            close (pipefd[0]);
+            return pl->next;
+    }
+    return 0;
+}
 
 /*
  * execute_one_subcommand():
- * Do file redirections if applicable, then [you can fill this in...]
+ * Do file redirections if applicable, then execute the given command from p
  * Does not return, so you want to fork() before calling me.
  */
 void execute_one_subcommand(struct parsed_line *p)
 {
+    struct pipeline *pl;
     extern char **environ;
     struct stat buf;
     char *fileLoc;
 
     /* Getting the input if new input */
     if (p->inputfile) {
-    	close(0);
-    	if (open(p->inputfile, O_RDONLY, 0) < 0) {
-    	    perror(p->inputfile);
-    	    exit(1);
-    	}
+        close(0);
+        if (open(p->inputfile, O_RDONLY, 0) < 0) {
+            perror(p->inputfile);
+            exit(1);
+        }
     }
 
     /* Getting the output if new output */
     if (p->outputfile) {
-    	close(1);
+        close(1);
         if (open(p->outputfile, O_WRONLY|O_CREAT|O_TRUNC, 0666) < 0) {
-    	    perror(p->outputfile);
-    	    exit(1);
-	    }
+            perror(p->outputfile);
+            exit(1);
+        }
 
         /* If we are writing stderr and stdout to the same file */
         if (p->output_is_double){
@@ -96,25 +134,23 @@ void execute_one_subcommand(struct parsed_line *p)
             dup2(1, 2);
         }
     }
-    if (p->pl)
-        /***********************************************************************
-        * We are now executing the first command
-        * REMOVE THIS
-        * ----------------------------------------------------------------------*/
-        printf("execute %s\n", p->pl->argv[0]);
-        /**********************************************************************/
-
+    if ((pl = p->pl)){
+        /* If there is a pipe, execute the pipe */
+        if (pl->next){
+            pl = execute_pipe(pl);
+        }
+        
         /* finding the location of the executing cmd */
-        fileLoc = p->pl->argv[0];
+        fileLoc = pl->argv[0];
         if (!(strchr(fileLoc, '/'))){
-            fileLoc = efilenamecons("/bin", p->pl->argv[0]);
+            fileLoc = efilenamecons("/bin", pl->argv[0]);
             if (stat(fileLoc, &buf)){
-                fileLoc = efilenamecons("/usr/bin", p->pl->argv[0]);
+                fileLoc = efilenamecons("/usr/bin", pl->argv[0]);
                 if (stat(fileLoc, &buf)){
-                    fileLoc = efilenamecons(".", p->pl->argv[0]);
+                    fileLoc = efilenamecons(".", pl->argv[0]);
                     if (stat(fileLoc, &buf)){
                         fprintf(stderr, "%s: Command not found\n", 
-                            p->pl->argv[0]);
+                            pl->argv[0]);
                         exit(1);
                     }
                 }
@@ -122,9 +158,10 @@ void execute_one_subcommand(struct parsed_line *p)
         }
 
         /* executing the command */
-        if ((laststatus = execve(fileLoc, p->pl->argv, environ))){
+        if ((laststatus = execve(fileLoc, pl->argv, environ))){
             perror(fileLoc);
             exit(1);
         }
+    }
     exit(0);
 }
